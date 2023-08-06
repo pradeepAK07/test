88a7f6f23588f6e14279e27c2bb568bc85c3e2d5
pradeepAK07 <pradeep07ak@gmail.com>
2023-08-06 20:06:18 +0530

    name

---
+++ Example.js
@@ -0,0 +1,18 @@
+const obj = {
+  name: "tylerdurden",
+  details: function () {
+    console.log("name " + this.name);
+  },
+};
+
+const obj2 = {
+  name: "pradeep",
+};
+
+const obj3 = {
+  name: "law",
+  age: 21,
+};
+const value = obj.details.bind(obj2);
+
+value();
